334602d3649135dcd33f69b83645edfd7f4b8e71
Serban <103998296+c0bqlt@users.noreply.github.com>
2024-08-20 10:16:29 +0300

    fix window propblem

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Widget from "./Widget"; // Import your Widget component
 
-const rootElement = document.getElementById("app");
+window.onload = function () {
+  const rootElement = document.getElementById("app");
 
-if (rootElement) {
-  // Retrieve title prop from a global variable or from a data attribute on the root element
-  const title =
-    window.customWidgetTitle ||
-    rootElement.getAttribute("data-title") ||
-    "Default Title";
+  if (rootElement) {
+    const title =
+      window.customWidgetTitle ||
+      rootElement.getAttribute("data-title") ||
+      "Default Title";
 
-  // Define props for the Widget component
-  const widgetProps = {
-    title: title,
-  };
+    const widgetProps = {
+      title: title,
+    };
 
-  // Render the Widget component
-  ReactDOM.render(React.createElement(Widget, widgetProps), rootElement);
-}
+    ReactDOM.render(React.createElement(Widget, widgetProps), rootElement);
+  }
+};
