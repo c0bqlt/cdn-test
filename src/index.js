2477ec4f9f46df77cace75142682b6f54e38ce9d
Serban <103998296+c0bqlt@users.noreply.github.com>
2024-08-22 14:40:07 +0300

    fix imports, again

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ChatWidget from "./ChatWidget"; // Import your Widget component
 
+// Function to load an external CSS file by dynamically injecting a <link> tag
+function loadExternalCSS(url) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = url;
+  document.head.appendChild(link);
+}
+
+// Load FontAwesome from CDN
+loadExternalCSS("https://kit.fontawesome.com/b51492ca5f.css");
+
+// Load Google Fonts from CDN
+loadExternalCSS(
+  "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+);
+
 class ChatBotElement extends HTMLElement {
   connectedCallback() {
     // This method is called when the element is added to the DOM
